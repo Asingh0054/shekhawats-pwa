c892f339d39b09f06ecd70826037bc89447cdbe6
Asingh0054 <andysfarm12@gmail.com>
2025-11-07 10:18:59 +0530

    Fix service worker absolute path and offline cache

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,24 @@
-const CACHE = "shekhawats-cache-v1";
-const ASSETS = ["/shekhawats-pwa/","/shekhawats-pwa/index.html","/shekhawats-pwa/manifest.json"];
+const CACHE_NAME = "shekhawats-cache-v2";
+const ASSETS = [
+  "/shekhawats-pwa/",
+  "/shekhawats-pwa/index.html",
+  "/shekhawats-pwa/manifest.json",
+  "/shekhawats-pwa/images/icon-192x192.png",
+  "/shekhawats-pwa/images/icon-512x512.png"
+];
 
 self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
 self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+  );
 });
